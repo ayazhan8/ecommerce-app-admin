c574727888821a0e052db734fdb3c823a3245da9
ayazhan8 <ayerikkyzy@gmail.com>
2023-11-04 20:18:34 +0000

    add: toast notifications on store creation

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -18,6 +18,7 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 const formSchema = z.object({
   name: z.string().min(1),
@@ -37,14 +38,16 @@ export const StoreModal = () => {
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
-        setLoading(true);
-        const response = await axios.post('/api/stores', values);
-        console.log(response.data);
+      setLoading(true);
+
+      const response = await axios.post("/api/stores", values);
+
+      toast.success("Store created.");
     } catch (error) {
-        console.log(error);
-        
+      console.log(error);
+      toast.error("Something went wrong.");
     } finally {
-        setLoading(false);
+      setLoading(false);
     }
   };
 
