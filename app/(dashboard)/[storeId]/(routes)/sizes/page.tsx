13559c163a58ed6fa41e09c52956afff9f70843a
ayazhan8 <ayerikkyzy@gmail.com>
2023-12-22 14:51:55 +0000

    add Sizes entity

---
+++ app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
@@ -0,0 +1,36 @@
+import prismadb from "@/lib/prismadb";
+
+import { SizeColumn } from "./components/columns"
+import { SizesClient } from "./components/client";
+
+const SizesPage = async ({
+  params
+}: {
+  params: { storeId: string }
+}) => {
+  const sizes = await prismadb.size.findMany({
+    where: {
+      storeId: params.storeId
+    },
+    orderBy: {
+      createdAt: 'desc'
+    }
+  });
+
+  const formattedSizes: SizeColumn[] = sizes.map((item) => ({
+    id: item.id,
+    name: item.name,
+    value: item.value,
+    createdAt: item.createdAt.toLocaleDateString(),
+  }));
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <SizesClient data={formattedSizes} />
+      </div>
+    </div>
+  );
+};
+
+export default SizesPage;
